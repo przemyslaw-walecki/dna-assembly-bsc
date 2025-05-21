b6e136567bfdbcdac64c7c5a7ac9df77ff905e75
przemyslaw-walecki <p.walecki12@gmail.com>
2025-05-21 22:48:02 +0200

    Cleanup Before Rust

--- basic_dna_assembler/compare.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-poetry run compare-assemblies --data-dir ./data --reference ./data/GCF_000007365.1_ASM736v1_genomic.fna --assemblers dnaasm --output report.csv --md-log comparison.log.md
