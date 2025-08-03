436feee200ebef6a1ccfc04621597645b86cd1a9
przemyslaw-walecki <p.walecki12@gmail.com>
2025-08-03 13:50:18 +0200

    Results of bubbles detection

--- generate_reads.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm \
-  -v "$(pwd)/random_genome_generator:/data" \
-  -w /data \
-  biocontainers/pirs:v2.0.2dfsg-8-deb_cv1 \
-  sh -c "pirs simulate random_50.fna -x 200 -l 35 -m 35 -v 2 -e 0.00 -o reads && mv reads_1.fq reads50_v2.fq"
