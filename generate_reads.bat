cc415ab3272c39865de887d2d46855db8eed7ae8
przemyslaw-walecki <p.walecki12@gmail.com>
2025-11-08 23:07:32 +0100

    GNN hyperparameters sweep results

--- generate_reads.bat
+++ generate_reads.bat
@@ -2,4 +2,4 @@ docker run --rm ^
   -v "C:\Users\Przemek\Desktop\Inzynierka stuff\own_assembler\dna-assembly-bsc\genomes\:/data" ^
   -w /data ^
   biocontainers/pirs:v2.0.2dfsg-8-deb_cv1 ^
-  sh -c "pirs simulate Klebsiella_pneumoniae_subsp_pneumoniae_NTUH-K2044_DNA.fasta -x 20 -l 100 -m 300 -v 2 -e 0.00 -o klebsiella_clean_cov20"
+  sh -c "pirs simulate Klebsiella_pneumoniae_subsp_pneumoniae_NTUH-K2044_DNA.fasta -x 30 -l 100 -m 300 -v 2 -e 0.00 -o klebsiella_clean_cov30"
--- rust_basic_dna_assembler/assemble.sh
+++ rust_basic_dna_assembler/assemble.sh
@@ -1,4 +1,5 @@
  #!/bin/sh
-
- ./target/release/rust_basic_dna_assembler -1 ../genomes/salmonella_clean_cov20_100_300_1.fq -2 ../genomes/salmonella_clean_cov20_100_300_2.fq -k 21 -t 0 -d 0 -o salmonella_assembled_k21.fa
+cargo clean 
+cargo build --release
+ ./target/release/rust_basic_dna_assembler -1 ../genomes/klebsiella_clean_cov10_100_300_1.fq -2 ../genomes/klebsiella_clean_cov10_100_300_2.fq -k 21 -t 0 -d 0 -o klebsiella_k21_cov10_graph.gfa
  
